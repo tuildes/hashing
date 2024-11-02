e5965376897311287443469c6116b3d09cfce85c
Gustavo Frehse <frehsegustavo@gmail.com>
2024-11-02 13:07:22 +0000

    feat: search function para hash

--- src/hash.c
+++ src/hash.c
@@ -5,9 +5,10 @@ size_t h1(int key) {
     return (size_t)(key % HASH_TABLE_SIZE); 
 }
 
-// ⌊m * (k * 0.9 − ⌊k * 0.9⌋)⌋
 size_t h2(int key) {
-    return (size_t)floor((HASH_TABLE_SIZE * ((key * 0.9) - floor((key * 0.9)))));
+    if (key < 0) key *= (-1); // Tratamento de negativos
+    return (size_t)
+        floor((HASH_TABLE_SIZE * ((key * 0.9) - floor((key * 0.9)))));
 }
 
 Hash_t create_hash_table() {
@@ -21,10 +22,22 @@ Hash_t create_hash_table() {
     return h;
 }
 
+int search_hash_table(Hash_t t, int k) {
+    size_t pos = h1(k);
+
+    if((t.h1[pos].started) && (!t.h1[pos].deleted) && (t.h1[pos].value == k))
+        return 1;
+    if((t.h2[pos].started) && (!t.h2[pos].deleted) && (t.h2[pos].value == k))
+        return 2;
+        
+    return 0;
+}
+
 void insert_hash_table(Hash_t *t, int k) {
     
-    size_t pos = h1(k), j;
+    size_t j, pos = h1(k);
 
+    // Verificar se existe chave na posicao 01
     if((t->h1[pos].started) && (!t->h1[pos].deleted)) {
         // Copiar a T1 para T2
         j = h2(t->h1[pos].value);
@@ -33,6 +46,7 @@ void insert_hash_table(Hash_t *t, int k) {
         t->h2[j].deleted = 0;
     }
 
+    // Inserir a chave na tabella
     t->h1[pos].value = k;
     t->h1[pos].started = 1;
     t->h1[pos].deleted = 0;
@@ -40,7 +54,7 @@ void insert_hash_table(Hash_t *t, int k) {
 
 int delete_hash_table(Hash_t *t, int k) {
 
-    // Deleta primeiro da segunda tabela
+    // Deleta primeiro na segunda tabela (se corresponder)
     size_t pos = h2(k);
     if ((t->h2[pos].started) && ((t->h2[pos].value == k))) {
         t->h2[pos].deleted = 1;
@@ -48,6 +62,7 @@ int delete_hash_table(Hash_t *t, int k) {
         return (int)pos;
     }
 
+    // Verifica se existe na primeira tabela e deleta
     pos = h1(k);
     if((t->h1[pos].started) && (t->h1[pos].value == k)) {
         t->h1[pos].deleted = 1;
@@ -60,6 +75,7 @@ int delete_hash_table(Hash_t *t, int k) {
 
 void print_hash_table(Hash_t t) {
 
+    // Estrutura de dados temporario para ordenacao
     struct __num_table {
         int value;
         size_t level;
@@ -67,11 +83,13 @@ void print_hash_table(Hash_t t) {
     };
 
     // Acha o tamanho da tabela (nao vazios)
-    size_t size = 0;
-    struct __num_table v[(HASH_TABLE_SIZE << 1)];
-    struct __num_table temp;
+    size_t size = 0; // Quantidade de dados validos
+    struct __num_table v[(HASH_TABLE_SIZE << 1)]; // Array de ordenacao
+    struct __num_table temp; // Dado temporario para trocas
 
+    // Armazenar dados validos nas tabelas
     for(size_t i=0; i < HASH_TABLE_SIZE; i++) {
+
         // Conteudo da primeira tabela
         if ((t.h1[i].started) && (!t.h1[i].deleted)) {
             v[size].value = t.h1[i].value;
@@ -100,7 +118,7 @@ void print_hash_table(Hash_t t) {
     }
 
     // Imprimir toda as saidas
-    for(size_t i=0; i < size; i++) {
+    // (numero,tabela,posicao)
+    for(size_t i=0; i < size; i++)
         printf("%d,T%1hu,%ld\n", v[i].value, (v[i].table + 1) , v[i].level);
-    }
 }
--- src/hash.h
+++ src/hash.h
@@ -1,32 +1,78 @@
 #ifndef __HASH_H__
 #define __HASH_H__
 
-#define HASH_TABLE_SIZE 11
-
 #include <stdio.h>
 #include <math.h>
 
-// Struct de numeros para a hash
+#define HASH_TABLE_SIZE 11 // Tamanho de cada tabela HASH
+
+// Estrutura de numeros da hash
 typedef struct {
-    int value;
-    unsigned int deleted:1,
-                 started:1;
+    int value; // Valor qualquer (chave)
+    unsigned int deleted:1, // Bit de delete
+                 started:1; // Bit de numero "iniciado"
 } __hash_number;
 
+// Estrutura de dados do tipo HASH
+// Tabela Hash do tipo Cuckoo hashing
 typedef struct {
-    __hash_number h1[HASH_TABLE_SIZE],
-                  h2[HASH_TABLE_SIZE];
+    __hash_number h1[HASH_TABLE_SIZE], // Tabela 01 (esquerda)
+                  h2[HASH_TABLE_SIZE]; // Tabela 02 (direita)
 } Hash_t;
 
-size_t h1(int key);
-size_t h2(int key);
+/*
+    Cria uma chave para a tabela 01 ou 02 de Hash
 
+    @param  (size_t) key - chave que vai ser indexado
+    @return (size_t) chave de indexacao na tabela
+*/
+size_t h1(int key); // Indexacao do tipo 01 (modulo simples)
+size_t h2(int key); // Indexacao do tipo 02 (diferencas de pisos)
+
+/*
+    Cria a tabela hash
+
+    @return (Hash_t) - tabela hash criada
+*/
 Hash_t create_hash_table();
 
+/*
+    Procura o valor na tabela hash
+    Retorna 0 para nao encontrado
+    Retorna 1 para tabela 01
+    Retorna 2 para tabela 02
+
+    @param  (size_t) key - chave que vai ser procurado
+    @return (int)    qual tabela esta inserido
+*/
+int search_hash_table(Hash_t t, int k);
+
+/*
+    Insere uma chave na tabela HASH
+    Insere sempre na T1 da Hash
+
+    @param  (Hash_t) t - tabela hash a ser inserida
+    @param  (int)    k - chave a ser inserida
+*/
 void insert_hash_table(Hash_t *t, int k);
 
+/*
+    Deleta uma chave da tebela HASH
+
+    @param  (Hash_t) t - tabela hash a ser mexida
+    @param  (int)    k - chave a ser deletada
+    @return (int)    Posicao da HASH deletado, ou -1 para nao encontrado
+*/
 int delete_hash_table(Hash_t *t, int k);
 
+
+/*
+    Imprime de forma ordenado a tabela, da forrma:
+    (numero, tabela, posicao)
+    (10,T2,4)
+
+    @param  (Hash_t) t - tabela hash a ser impressa
+*/
 void print_hash_table(Hash_t t);
 
 #endif // __HASH_H__
